d786b1af3e2ece99f00027aa73b3dab8a348e325
alisher-erbolat <alisher.001017@gmail.com>
2023-07-07 23:43:15 +0600

    fix

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,58 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class FileReader {
+    public Profile getDataFromFile(File file) throws IOException {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+        String fileData = readFileData(file);
+        return parseProfileData(fileData);
+    }
+
+    private String readFileData(File file) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file,
+                "rw");
+        FileChannel fileChannel = randomAccessFile.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        Charset charset = StandardCharsets.US_ASCII;
+        while (fileChannel.read(byteBuffer) > 0) {
+            byteBuffer.rewind();
+            stringBuilder.append(charset.decode(byteBuffer)).append(System.lineSeparator());
+            byteBuffer.flip();
+        }
+        fileChannel.close();
+        randomAccessFile.close();
+        return stringBuilder.toString();
+    }
+
+    private Profile parseProfileData(String fileData) {
+        String[] lines = fileData.split(System.lineSeparator());
+        String name = null;
+        int age = 0;
+        String email = null;
+        Long phone = null;
+
+        for (String line : lines) {
+            if (line.startsWith("Name:")) {
+                name = line.substring(line.indexOf(":") + 1).trim();
+            } else if (line.startsWith("Age:")) {
+                age = Integer.parseInt(line.substring(line.indexOf(":") + 1).trim());
+            } else if (line.startsWith("Email:")) {
+                email = line.substring(line.indexOf(":") + 1).trim();
+            } else if (line.startsWith("Phone:")) {
+                phone = Long.parseLong(line.substring(line.indexOf(":") + 1).trim());
+            }
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
+
